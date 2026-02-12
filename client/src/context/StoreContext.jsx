2e6790d8cd7d9d80b92800ddd92b9fdf087fc5d6
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-02-12 07:16:35 +0530

    create the order model

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -8,16 +8,33 @@ const [cartItems, setCartItems] = useState({});
 const [food_list, setFoodList] = useState([]);
 const [token,setToken]=useState("");
 const url="http://localhost:4000"
-const addToCart = (itemId) => {
+
+const addToCart =async (itemId) => {
   if (!cartItems[itemId]) {
     setCartItems((prev) => ({ ...prev, [itemId]:1 }));
   } else {
     setCartItems((prev) => ({ ...prev, [itemId]:prev[itemId]+1 }));
   }
+  if (token) {
+  await axios.post(
+    url + "/api/cart/add",
+    { itemId },
+    { headers: { token } }
+  );
+}
+
 };
 
-const removeFromCart = (itemId) => {
+const removeFromCart = async(itemId) => {
   setCartItems((prev) => ({ ...prev, [itemId]:prev[itemId] - 1 }));
+  if (token) {
+  await axios.post(
+    url + "/api/cart/remove",
+    { itemId },
+    { headers: { token } }
+  );
+}
+
 };
 
 
@@ -41,6 +58,15 @@ const fetchFoodList = async () => {
   setFoodList(response.data.data);
 };
 
+const loadCartData = async (token) => {
+  const response = await axios.post(
+    url + "/api/cart/get",
+    {},
+    { headers: { token } }
+  );
+  setCartItems(response.data.cartData);
+};
+
 
 
 useEffect(() => {
@@ -49,6 +75,7 @@ useEffect(() => {
 
     if (localStorage.getItem("token")) {
       setToken(localStorage.getItem("token"));
+      await loadCartData(localStorage.getItem("token"));
     }
   };
 
