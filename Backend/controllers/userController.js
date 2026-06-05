dcd4dfba61f60b57ee75de4f4f4d5803178e8a05
N-Viveharan <nagalingamviveharan2001@gmail.com>
2026-06-05 22:38:49 +0530

    user contrl

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
 
+
+
 // login user
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
